f48c6c2b6b5798f03c7308760b41dd3d01bdeeea
Enio Jr <eniojr100@gmail.com>
2026-05-24 16:22:13 -0300

    :wrench: chore: setup inicial do projeto com Spring Boot e config do banco Neon

---
+++ src/main/java/br/edu/ifal/sigamais/SigamaisApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifal.sigamais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigamaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigamaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifal/sigamais/SigamaisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifal.sigamais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigamaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
